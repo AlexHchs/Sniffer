3f63fe098cbbcbf52112e5a5611102dca5a12e0b
310555021 <hhs.alex.cs10@nycu.edu.tw>
2022-12-27 23:54:25 +0800

    (Server/Client)

--- client.py
+++ client.py
@@ -57,6 +57,7 @@ def main():
                 os.remove(os.path.join(shared_directory, filename))
                 print('Finish transport file')
             break
+        
         except Exception:
             print(Exception)
             break
--- server.py
+++ server.py
@@ -17,50 +17,50 @@ def main():
         connection, address = server.accept()
         print("Connection success......")
         try:
-            while True:
-                print("Wait for receiving traffic data")
-                # Receive file numbers
-                receive = connection.recv(8096)
-                if not receive: break
+            print("Wait for receiving traffic data")
+            # Receive file numbers
+            receive = connection.recv(8096)
+            if not receive: break
 
-                # Parse the command
-                command = receive.decode('utf-8').split()
-                file_number = command[0]
+            # Parse the command
+            command = receive.decode('utf-8').split()
+            file_number = command[0]
                 
-                for i in range(int(file_number)):
-                    # Open a new file as writing, then write the new file from the server host
-                    # First step: receive the header size
-                    object = connection.recv(4)
-                    header_size = struct.unpack('i', object)[0]
+            for i in range(int(file_number)):
+                # Open a new file as writing, then write the new file from the server host
+                # First step: receive the header size
+                object = connection.recv(4)
+                header_size = struct.unpack('i', object)[0]
 
-                    # Second step: receive the header
-                    header_bytes = connection.recv(header_size)
+                # Second step: receive the header
+                header_bytes = connection.recv(header_size)
 
-                    # Third step: extract the detail info. of the real data from header
-                    header_json = header_bytes.decode('utf-8')
-                    header_dic = json.loads(header_json)
-                    '''
-                    header_dic = {
-                        'filename': filename,
-                        'file_size': os.path.getsize('%s/%s' % (shared_directory, filename))
-                    }
-                    '''
-                    total_size = header_dic['file_size']
-                    file_name = header_dic['filename']
+                # Third step: extract the detail info. of the real data from header
+                header_json = header_bytes.decode('utf-8')
+                header_dic = json.loads(header_json)
+                '''
+                header_dic = {
+                    'filename': filename,
+                    'file_size': os.path.getsize('%s/%s' % (shared_directory, filename))
+                }
+                '''
+                total_size = header_dic['file_size']
+                file_name = header_dic['filename']
 
-                    # Fourth step: receive the real data
-                    try:
-                        f = open('%s/%s' % (download_directory, file_name), 'wb')
-                        recv_size = 0
-                        while recv_size < total_size:
-                            line = connection.recv(1024)
-                            f.write(line)
-                            recv_size += len(line)
-                            print('Total size: %s, Already downloads: %s' % (total_size, recv_size))
-                        print('Finish download file')
-                        f.close()
-                    except:
-                        print('Fail to open the file')
+                # Fourth step: receive the real data
+                try:
+                    f = open('%s/%s' % (download_directory, file_name), 'wb')
+                    recv_size = 0
+                    while recv_size < total_size:
+                        line = connection.recv(1024)
+                        f.write(line)
+                        recv_size += len(line)
+                        print('Total size: %s, Already downloads: %s' % (total_size, recv_size))
+                    print('Finish download file')
+                    f.close()
+                except:
+                    print('Fail to open the file')
+            connection.close()
 
         except Exception:
             print(Exception)
