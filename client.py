1304d68075c6d4ba488346ed6e21e6ff641ae99a
310555021 <hhs.alex.cs10@nycu.edu.tw>
2022-12-27 15:37:15 +0800

    Update(Server/Client)

--- client.py
+++ client.py
@@ -12,41 +12,41 @@ def main():
     PORT = 9091
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((HOST, PORT))
+    print("Connect sucess......")
 
     while True:
-        connection, address = client.accept()
-        while True:
-            try: 
-                # Receive command
-                receive = connection.recv(8096)
-                if not receive: break # For Linux OS
+        try: 
+            # Receive command
+            receive = client.recv(8096)
+            if not receive: continue # For Linux OS
+            print(receive)
 
-                # Parse command and extract the related command parameters
-                commands = receive.decode('utf-8').split()
-                filename = commands[1]
+            # Parse command and extract the related command parameters
+            commands = receive.decode('utf-8').split()
+            filename = commands[1]
+            print(filename)
 
-                # Open the file as the read, send the content back to the client host
-                # First step: make the static size header
-                header_dic = {
-                    'filename': filename,
-                    'file_size': os.path.getsize('%s/%s' % (shared_directory, filename))
-                }
-                header_json = json.dumps(header_dic)
-                header_bytes = header_json.encode('utf-8')
+            # Open the file as the read, send the content back to the client host
+            # First step: make the static size header
+            header_dic = {
+                'filename': filename,
+                'file_size': os.path.getsize('%s/%s' % (shared_directory, filename))
+            }
+            header_json = json.dumps(header_dic)
+            header_bytes = header_json.encode('utf-8')
 
-                # Second step: send the header's size
-                connection.send(struct.pack('i', len(header_bytes)))
+            # Second step: send the header's size
+            client.send(struct.pack('i', len(header_bytes)))
 
-                # Third step: send the header
-                connection.send(header_bytes)
+            # Third step: send the header
+            client.send(header_bytes)
 
-                # Fourth step: send the real file
-                with open('%s/%s' % (shared_directory, filename), 'rb') as f:
-                    for line in f:
-                        connection.send(line)
-            except:
-                break
-        connection.close()
+            # Fourth step: send the real file
+            with open('%s/%s' % (shared_directory, filename), 'rb') as f:
+                for line in f:
+                    client.send(line)
+        except:
+            break
     client.close()
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -12,40 +12,43 @@ def main():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((HOST, PORT))
     server.listen(5)
-
     while True:
-        command = input('>>: ').strip()
-        if not command: continue
-        server.send(command.encode('utf-8'))
+        connection, address = server.accept()
+        while True:
+            command = input('>>: ').strip()
+            if command == 'exit': break
+            
+            connection.send(command.encode('utf-8'))
 
-        # Open a new file as writing, then write the new file from the server host
-        # First step: receive the header size
-        object = server.recv(4)
-        header_size = struct.unpack('i', object)[0]
+            # Open a new file as writing, then write the new file from the server host
+            # First step: receive the header size
+            object = connection.recv(4)
+            header_size = struct.unpack('i', object)[0]
 
-        # Second step: receive the header
-        header_bytes = server.recv(header_size)
+            # Second step: receive the header
+            header_bytes = connection.recv(header_size)
 
-        # Third step: extract the detail info. of the real data from header
-        header_json = header_bytes.decode('utf-8')
-        header_dic = json.loads(header_json)
-        '''
-        header_dic = {
-            'filename': filename,
-            'file_size': os.path.getsize('%s/%s' % (shared_directory, filename))
-        }
-        '''
-        total_size = header_dic['file_size']
-        file_name = header_dic['filename']
+            # Third step: extract the detail info. of the real data from header
+            header_json = header_bytes.decode('utf-8')
+            header_dic = json.loads(header_json)
+            '''
+            header_dic = {
+                'filename': filename,
+                'file_size': os.path.getsize('%s/%s' % (shared_directory, filename))
+            }
+            '''
+            total_size = header_dic['file_size']
+            file_name = header_dic['filename']
 
-        # Fourth step: receive the real data
-        with open('%s/%s' % (download_directory, file_name), 'wb') as f:
-            recv_size = 0
-            while recv_size < total_size:
-                line = server.recv(1024)
-                f.write(line)
-                recv_size += len(line)
-                print('Total size: %s, Already downloads: %s' % (total_size, recv_size))
+            # Fourth step: receive the real data
+            with open('%s/%s' % (download_directory, file_name), 'wb') as f:
+                recv_size = 0
+                while recv_size < total_size:
+                    line = server.recv(1024)
+                    f.write(line)
+                    recv_size += len(line)
+                    print('Total size: %s, Already downloads: %s' % (total_size, recv_size))
+            connection.close()
         server.close()
 
 if __name__ == "__main__":
